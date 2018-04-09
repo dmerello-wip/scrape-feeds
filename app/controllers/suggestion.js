52e8f0e9ee224a1596597e6d85657b6c2dd638d2
Diego Merello <diego@wipitalia.it>
2018-04-09 22:08:25 +0200

    cominciamo a ripulire il controller creando una classe

--- app/controllers/suggestion.js
+++ app/controllers/suggestion.js
@@ -1,55 +1,62 @@
-const appConfig = require('../config.js');
 const suggestionMdl = require('./../models/suggestion');
 const tagMdl = require('./../models/tag');
 const Validator = require('./../helpers/validator');
 
-exports.createSuggestion = function(req, res) {
-    // data to post:
-    let postData = {
-        title : req.body.title,
-        description : req.body.description,
-        file : req.file.filename
-    };
-    let tags = req.body.tags.split(',');
 
-    // Validate Suggestions data
-    let validation = new Validator(postData, suggestionMdl.config());
-    console.log(validation.messages);
-    // TODO: validate tags data
+class SuggestionCtrl {
+
+    createSuggestion(req, res) {
+        // data to post:
+        let postData = {
+            title: req.body.title,
+            description: req.body.description,
+            file: req.file.filename
+        };
+        let tags = req.body.tags.split(',');
+
+        // Validate Suggestions data
+        let validation = new Validator(postData, suggestionMdl.config());
+        console.log(validation.messages);
+        // TODO: validate tags data
 
 
-    // create
-    // TODO: rendere leggibili sti annidamenti dividendo
-    suggestionMdl.create( postData , function(status, msg){
-        if(!status) {
-            res.json({'code' : 400, 'message' : msg});
-        } else {
-            let id = (msg.insertId);
-            console.log(`created Suggestion id: ${id}`);
-            for(let tagName of tags){
-                tagMdl.getTagId(tagName, function(tagId){
-                    if (status) {
-                        suggestionMdl.relateToTag(id, tagId, function(status, msg){
-                            if (!status) {
-                                res.json({'code' : 400, 'message' : msg});
-                            }
-                        });
-                    } else {
-                        res.json({'code' : 400, 'message' : msg});
-                    }
-                });
+        // create
+        // TODO: rendere leggibili sti annidamenti dividendo
+        suggestionMdl.create(postData, function (status, msg) {
+            if (!status) {
+                res.json({ 'code': 400, 'message': msg });
+            } else {
+                let id = (msg.insertId);
+                console.log(`created Suggestion id: ${id}`);
+                for (let tagName of tags) {
+                    tagMdl.getTagId(tagName, function (tagId) {
+                        if (status) {
+                            suggestionMdl.relateToTag(id, tagId, function (status, msg) {
+                                if (!status) {
+                                    res.json({ 'code': 400, 'message': msg });
+                                }
+                            });
+                        } else {
+                            res.json({ 'code': 400, 'message': msg });
+                        }
+                    });
+                }
+                res.json({ 'code': 200, 'message': msg });
             }
-            res.json({'code' : 200, 'message' : msg});
-        }
-    });
+        });
 
-};
+    };
 
-exports.updateSuggestion = function(req, res) {
-    suggestionMdl.update(req, res, {
-        id : req.body.id,
-        title : req.body.title,
-        description : req.body.description,
-        file : req.file.filename
-    });
-};
+    updateSuggestion(req, res) {
+        suggestionMdl.update(req, res, {
+            id: req.body.id,
+            title: req.body.title,
+            description: req.body.description,
+            file: req.file.filename
+        });
+    };
+
+}
+
+
+module.exports = new SuggestionCtrl();
