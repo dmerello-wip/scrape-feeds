cb5b41abb61ed70cdc850b19424e373933a892d6
Diego Merello <diego@wipitalia.it>
2018-04-29 18:41:44 +0200

    fixed url validator

--- app/helpers/validator.js
+++ app/helpers/validator.js
@@ -158,14 +158,15 @@ class Validator {
         return !isNaN(value)
     }
 
-    checkUrl(value) {
-        let pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+    checkUrl(str) {
+        var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name and extension
             '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-            '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+            '(\\:\\d+)?'+ // port
+            '(\\/[-a-z\\d%@_.~+&:]*)*'+ // path
+            '(\\?[;&a-z\\d%@_.,~+&:=-]*)?'+ // query string
             '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-        return pattern.test(value);
+        return pattern.test(str);
     }
 
     checkString(value) {
